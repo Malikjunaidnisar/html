847f04d767f410bb788b60684893c543372eacb0
Malikjunidnisar <sayaandtrain1@gmail.com>
2025-02-04 11:53:14 +0500

    javascript

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+fullname="junaid"
+age=28
+Age=28.3
+Pakistani=true
+isnational=null
+abc="" //primitive data type
+
+Object={
+    fullname:"junaid",
+    age:28,
+    Age:2.0,
+    Pakistani:true,
+    isnational:null
+} //non-primitive data type object data type
+arr=[1,2,3.5] //non-primitive data type array data type
+{
+    const fname=50
+console.log(fname)
+}
+console.log(Object.age,Object.fullname,Pakistani,arr[2]) //
