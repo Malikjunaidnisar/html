7af6ee8950b4757dd76fec17bc4f694b7e6f83b2
Malikjunidnisar <sayaandtrain1@gmail.com>
2025-02-06 12:02:49 +0500

    javascript

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,32 @@ Pakistani=true
 isnational=null
 abc="" //primitive data type
 
-Object={
+Object1={
     fullname:"junaid",
     age:28,
     Age:2.0,
     Pakistani:true,
-    isnational:null
+    isnational:null,
+    name:"malik"
 } //non-primitive data type object data type
+Object2=Object1
+Object1.age=29
 arr=[1,2,3.5] //non-primitive data type array data type
+arr1=arr
+arr[3]=12345798
 {
     const fname=50
-console.log(fname)
+//console.log(fname)
 }
-console.log(Object.age,Object.fullname,Pakistani,arr[2]) //
+//console.log(Object.age,Object.fullname,Pakistani,arr[2])
+//console.log(arr)
+//console.log(arr1)
+//console.log(Object1)
+//console.log(Object2)
+var a = 1+2 // math operator for addition
+var b=5-10  // math operator for subtraction
+var c=8*3 // math operator for multiplying
+var d =9/3 // math operator for division
+var e=4%3 // math operator for modulus(reminder: division)
+var f=2**5 // math operator for exponentiation(power of any number)
+console.log(a,b,c,d,e,f)
