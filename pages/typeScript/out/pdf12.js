376946bdeba5653e170adac27fac6ce8d05fef33
Malikjunidnisar <sayaandtrain1@gmail.com>
2025-03-25 12:05:43 +0500

    lernising

---
+++ pages/typeScript/out/pdf12.js
@@ -0,0 +1,9 @@
+"use strict";
+let age = 18;
+let canVote = age >= 18 ? "yes" : "no";
+console.log(canVote);
+function isEven(value) {
+    let answer = value % 2 == 0 ? true : false;
+    console.log(answer);
+}
+isEven(5);
---
+++ pages/typeScript/src/pdf12.ts
@@ -0,0 +1,8 @@
+let age:number =18;
+let canVote:string=age>=18?"yes":"no"
+console.log(canVote)
+function isEven (value:number){
+    let answer=value%2==0? true:false
+    console.log(answer)
+}
+isEven(5)
