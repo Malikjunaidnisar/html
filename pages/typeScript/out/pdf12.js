36dce615a4e3d7053002c077e47fbf31eaf81d17
Malikjunidnisar <sayaandtrain1@gmail.com>
2025-03-27 12:05:48 +0500

    class 16

--- pages/typeScript/out/pdf12.js
+++ pages/typeScript/out/pdf12.js
@@ -1,9 +1,81 @@
 "use strict";
 let age = 18;
 let canVote = age >= 18 ? "yes" : "no";
-console.log(canVote);
+// console.log(canVote)
 function isEven(value) {
     let answer = value % 2 == 0 ? true : false;
+    return answer;
     console.log(answer);
 }
-isEven(5);
+// isEven(5)
+const colors = ["yellow", "green", "purple"];
+const favColors = ["pink", "orange"];
+const lightColors = 'white';
+const allColor = [...colors, lightColors, ...favColors];
+const User = {
+    id: 1,
+    name: "malik",
+    email: "sayaandtrain@gmail.com",
+    age: 28
+};
+// console.log(User)
+const newUser = Object.assign(Object.assign({}, User), { email: 'junaid@', age: 31 });
+// console.log(newUser)
+// console.log(usama1)
+function sum(a, b, c) {
+    return a + b + c;
+}
+const number1 = [1, 2, 3];
+// console.log(sum(...number1))
+function mergeArrays(arr1, arr2) {
+    let a = [...arr1, ...arr2];
+    return a;
+}
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+// console.log(mergeArrays(arr1,arr2))
+let p1 = ["red", "blue"];
+let p2 = p1;
+p1.push('orange');
+// console.log(p1)
+// console.log(p2)
+function bio(...args) {
+    console.log(args);
+    return args;
+}
+// console.log(bio('n','i','s'))
+function sum1(...numbers1) {
+    let sum = 0;
+    numbers1.forEach((num) => {
+        sum += num;
+    });
+    return sum;
+}
+// console.log(sum1(1,2,3,4))
+const shoppingart = [
+    { name: 'malik', price: 200 },
+    { name: "nisar", price: 300 },
+    { name: 'haris', price: 400 },
+];
+const result = shoppingart.reduce((accumulator, currentValue) => {
+    return (accumulator += currentValue.name);
+}, "");
+// console.log(result)
+let x ="abc"
+let result3 = x.reduce((accumulator,value)=>{
+    for (i=0;i < x.length;i++){
+        console.log(1)
+    }
+   
+},0)
+console.log(result3)
+// const data = {
+//     name: 'malik',
+//     age: 30,
+//     email: 'saya'
+// };
+// const userJSON = JSON.stringify(data);
+// console.log(userJSON);
+// localStorage.setItem('user', userJSON);
+// const str =localStorage.getItem("user")
+// console.log(str)
--- pages/typeScript/src/pdf12.ts
+++ pages/typeScript/src/pdf12.ts
@@ -1,8 +1,90 @@
 let age:number =18;
 let canVote:string=age>=18?"yes":"no"
-console.log(canVote)
-function isEven (value:number){
+// console.log(canVote)
+function isEven (value:number):boolean {
     let answer=value%2==0? true:false
+    return answer
     console.log(answer)
 }
-isEven(5)
+// isEven(5)
+const colors:string[]=["yellow","green","purple"]
+const favColors:string[]=["pink","orange"]
+const lightColors:string='white'
+const allColor:string[]=[...colors,lightColors,...favColors]
+// console.log(allColor)
+interface user {
+    id:number,
+    name:string,
+    email:string,
+    age:number
+}
+const User:user={
+    id:1,
+    name:"malik",
+    email:"sayaandtrain@gmail.com",
+    age:28
+}
+// console.log(User)
+const newUser:user={
+    ...User,
+    email:'junaid@',
+    age:31,
+    // 'f.name':"nisar"
+}
+// console.log(newUser)
+
+// console.log(usama1)
+function sum(a:number,b:number,c:number):number{
+    return a+b+c
+}
+const number1:[number,number,number]=[1,2,3]
+// console.log(sum(...number1))
+function mergeArrays (arr1:number[],arr2:number[]):number[]{
+    let a = [...arr1,...arr2]
+        return a
+}
+const arr1:number[]=[1,2,3]
+const arr2:number[]=[4,5,6]
+// console.log(mergeArrays(arr1,arr2))
+let p1:string[]=["red","blue"]
+let p2=p1
+p1.push('orange')
+// console.log(p1)
+// console.log(p2)
+function bio(...args:string[]):string[]{
+    console.log(args)
+    return args
+}
+// console.log(bio('n','i','s'))
+function sum1(...numbers1:number[]):number{
+    let sum =0
+    numbers1.forEach((num)=>{
+        sum += num
+    })
+    return sum
+}
+// console.log(sum1(1,2,3,4))
+const shoppingart=[
+    {name:'malik',price:200},
+    {name:"nisar",price:300},
+    {name:'haris',price:400},
+]
+const result = shoppingart.reduce((accumulator:string | number,currentValue)=>{
+    return (accumulator += currentValue.name)
+},"")
+
+// console.log(result)
+// let x ="abc"
+// let result3 = x.reduce((accumulator,value)=>{
+
+// },[])
+const data={
+    name:'malik',
+    age:30,
+    email:'saya'
+}
+const userJSON =JSON.stringify(data)
+console.log(userJSON)
+localStorage.setItem('user',userJSON)
+// const str =localStorage.getItem("user")
+// console.log(str)
